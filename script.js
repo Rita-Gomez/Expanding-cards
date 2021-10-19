ceabd6423dc9106da0153dd25aff666d51d0ab69
Rita-Gomez <ritagomezh@gmail.com>
2021-10-18 21:10:54 -0500

    Terminado

---
+++ script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel');
+// console.log(panels)
+
+// console.log(panels[1])
+panels.forEach(panel=>{
+    // console.log(panel)
+    panel.addEventListener('click',()=>{
+        removeActiveClasses();
+        // console.log(123)
+        panel.classList.add('active')
+    })
+})
+const removeActiveClasses = ()=>{
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
